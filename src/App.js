c3670a1afbc7e1260a08a1014ec79753ec741c73
md2eoseo <md2eoseo@gmail.com>
2020-11-11 12:01:18 +0900

    move duplicated container to App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
+import styled from "styled-components";
+
+const Main = styled.div`
+  width: 100%;
+  padding: 50px 10px;
+  display: grid;
+  row-gap: 20px;
+`;
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Header />
-        <Routes></Routes>
+        <Main>
+          <Routes></Routes>
+        </Main>
       </Router>
     </ThemeProvider>
   );
--- src/components/routes/Recent.js
+++ src/components/routes/Recent.js
@@ -1,20 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import RecentContainer from "../RecentContainer";
 
 function Recent() {
-  const Container = styled.div`
-    width: 100%;
-    padding: 50px 10px;
-    display: grid;
-    row-gap: 20px;
-  `;
-
-  return (
-    <Container>
-      <RecentContainer len={10} />
-    </Container>
-  );
+  return <RecentContainer len={10} />;
 }
 
 export default Recent;
